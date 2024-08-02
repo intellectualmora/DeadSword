b6ad1e487fe8d7a43ee2986b29485b2458cca93b
mora <64327020+intellectualmora@users.noreply.github.com>
2024-08-02 08:34:27 +0800

    add origin

---
+++ Source/DeadSword/CompDeadable.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace DS
+{
+    public class CompDeadable : ThingComp
+    {
+        private CompProperties_Dead Props => (CompProperties_Dead)this.props;
+
+        public override void Notify_KilledPawn(Pawn pawn)
+        {
+            base.Notify_KilledPawn(pawn);
+            GasUtility.AddDeadifeGas(pawn.Position, pawn.Map, pawn.Faction, this.Props.amount);
+        }
+    }
+}
---
+++ Source/DeadSword/CompProperties_Dead.cs
@@ -0,0 +1,11 @@
+﻿using Verse;
+
+namespace DS
+{
+    public class CompProperties_Dead : CompProperties
+    {
+        public int amount;
+
+        public CompProperties_Dead() => this.compClass = typeof(CompDeadable);
+    }
+}
